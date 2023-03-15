0101ea162ef3d2f4f65794f80a1a3c06b7deeeff
Arkadiy notebook <darwin227@yandex.ru>
2023-03-15 17:53:39 +0300

    v1

--- lab_1/lab_2.py
+++ lab_1/lab_2.py
@@ -1,6 +1,6 @@
 from lab_1 import *
-M = matrix_creat()
-D = vector_creat()
+M = [[10, 5, 0, 0, 0], [3, 10, -2, 0, 0], [0, 2, -9, -5, 0], [0, 0, 5, 16, -4], [0, 0, 0, -8, 16]]
+D = [-120, -91, 5, -74, -56]
 
 def progonka_matrix(matrix, vector):
     Q = []
@@ -21,7 +21,7 @@ def progonka_matrix(matrix, vector):
             B.append(matrix[i][i])
             C.append(0)
     for i in range(n):
-        if abs(B[i]) < abs(A[i]):
+        if abs(B[i]) < abs(A[i]) + abs(C[i]):
             print('достаточное условие не выполняется')
             quit()
     P[0] = -C[0] / B[0]
@@ -34,6 +34,16 @@ def progonka_matrix(matrix, vector):
     while v != 0:
         v -= 1
         X[v] = P[v] * X[v+1] + Q[v]
-    return X
+    print('Значения P')
+    for i in range(n):
+        print("%.1f" % P[i], end = " ")
+    print()
+    print('Значения Q')
+    for i in range(n):
+        print("%.1f" % Q[i], end = " ")
+    print()
+    print('Значения X')
+    for i in range(n):
+        print("%.1f" % X[i], end = " ")
 
-print(progonka_matrix(M, D))
+progonka_matrix(M, D)
